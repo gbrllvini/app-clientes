6394050bbc92a620ebff9fc07d4bd0a1e808bc3f
gbrllvini <gabvini2010@hotmail.com>
2024-02-19 15:06:53 -0300

    feat (Cadastro): cria classe cliente

---
+++ Cliente.cs
@@ -0,0 +1,10 @@
+namespace Cadastro;
+
+public class Cliente
+{
+    public int Id { get; set; }
+    public string? Name { get; set; }
+    public DateOnly Birthdate { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public decimal Desconto { get; set; }
+}
