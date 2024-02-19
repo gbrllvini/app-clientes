0ab69dd55bd09c740986a4ddcba27782cd845a9a
gbrllvini <gabvini2010@hotmail.com>
2024-02-19 15:11:58 -0300

    feat (Menu): implementa e executa menu

--- Program.cs
+++ Program.cs
@@ -4,6 +4,28 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        while (true)
+        {
+            Menu();
+
+            Console.ReadKey();
+        }
     }
+
+
+    static void Menu()
+    {
+        Console.Clear();
+
+        Console.WriteLine("Cadastro de Clientes");
+        Console.WriteLine("--------------------");
+        Console.WriteLine("1 - Cadastrar Cliente");
+        Console.WriteLine("2 - Exibir Clientes");
+        Console.WriteLine("3 - Editar Cliente");
+        Console.WriteLine("4 - Excluir Cliente");
+        Console.WriteLine("5 - Sair");
+        Console.WriteLine("--------------------");
+    }
+
+
 }
